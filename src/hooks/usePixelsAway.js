988cde3d01a457f4d49a118248f6a7439e31e6f3
David <davidcornejo1@live.com>
2022-09-29 20:44:52 -0600

    Added resize event listener

--- src/hooks/usePixelsAway.js
+++ src/hooks/usePixelsAway.js
@@ -7,10 +7,16 @@ function usePixelsAway() {
 
   useEffect(() => {
     const updateDistance = () => {
+      // Calculates how many pixels away the scrollbar is from the bottom of the page
       setPixelsAway(document.documentElement.scrollHeight - (window.scrollY + window.innerHeight));
     };
     window.addEventListener("scroll", updateDistance);
-    return () => window.removeEventListener("scroll", updateDistance);
+    // Resize event to update distance when Chrome's toolbar hides from view
+    window.addEventListener("resize", updateDistance);
+    return () => {
+      window.removeEventListener("scroll", updateDistance);
+      window.removeEventListener("resize", updateDistance);
+    };
   }, []);
 
   return pixelsAway;
