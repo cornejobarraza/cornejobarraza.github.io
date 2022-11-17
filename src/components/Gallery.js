5eea954789f08b02da51d27096f18a8dd8b37910
David <davidcornejo1@live.com>
2022-11-16 23:38:31 -0600

    Restored col size to 11

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,7 +5,7 @@ function Gallery() {
     <div className="row gap-5 justify-content-center text-center mx-auto">
       <div className="col-xl-3 col-lg-5">
         <div className="row gap-5 justify-content-evenly mx-auto">
-          <div className="col-10 about">
+          <div className="col-11 about">
             <h6 className="m-0">
               <i className="fa-solid fa-id-card"></i> User dashboard
             </h6>
@@ -36,7 +36,7 @@ function Gallery() {
       </div>
       <div className="col-xl-3 col-lg-5">
         <div className="row gap-5 justify-content-center mx-auto">
-          <div className="col-10 about">
+          <div className="col-11 about">
             <h6 className="m-0">
               <i className="fa-solid fa-spell-check"></i> English dictionary
             </h6>
@@ -67,7 +67,7 @@ function Gallery() {
       </div>
       <div className="col-xl-3 col-lg-5">
         <div className="row gap-5 justify-content-center mx-auto">
-          <div className="col-10 about">
+          <div className="col-11 about">
             <h6 className="m-0">
               <i className="fa-solid fa-chart-simple"></i> Expenses chart
             </h6>
