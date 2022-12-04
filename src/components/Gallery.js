2fc7b96e45b8d5ab42d462c832cb2ade2a8ee4ab
David <davidcornejo1@live.com>
2022-12-03 23:26:15 -0600

    Updated components for reusability

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,96 +3,42 @@ export { Gallery };
 function Gallery() {
   return (
     <div className="row gap-5 justify-content-center text-center mx-auto">
-      <div className="col-xl-3 col-lg-5">
-        <div className="row gap-5 justify-content-evenly mx-auto">
-          <div className="col-11 about">
-            <h6 className="m-0">
-              <i className="fa-solid fa-id-card"></i> User dashboard
-            </h6>
-          </div>
-          <div className="row justify-content-evenly gap-4">
-            <div className="col-5">
-              <img className="img-thumbnail" alt="Redux app screenshot" src="res/images/redux.webp" />
-            </div>
-            <div className="col-5" style={{ display: "grid", placeItems: "center", alignContent: "center" }}>
-              <h6 className="mb-3">CRUD app with Redux Toolkit and Firebase</h6>
-              <a href="redux" className="btn btn-sm" target="_blank" aria-label="Redux dashboard">
-                Test it
-              </a>
-            </div>
-            <div className="col mt-4 mb-1">
-              <a
-                className="btn"
-                style={{ fontSize: "0.8rem" }}
-                href="https://github.com/cornejobarraza/redux"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i className="fa-solid fa-code-branch" style={{ color: "var(--active)" }}></i> Repo
-              </a>
-            </div>
-          </div>
+      <Project name="User dashboard" description="CRUD app using Redux Toolkit and Firebase" repo="redux" />
+      <Project name="English dictionary" description="Dictionary based on a public API" repo="dictionary" />
+      <Project name="Expenses chart" description="Weekly expenses chart with dynamic data" repo="expenses" />
+    </div>
+  );
+}
+
+function Project({ name, description, repo }) {
+  return (
+    <div className="col-xl-3 col-lg-5">
+      <div className="row gap-5 justify-content-evenly mx-auto">
+        <div className="col-11 about">
+          <h6 className="m-0">
+            <i className="fa-solid fa-id-card"></i> {name}
+          </h6>
         </div>
-      </div>
-      <div className="col-xl-3 col-lg-5">
-        <div className="row gap-5 justify-content-center mx-auto">
-          <div className="col-11 about">
-            <h6 className="m-0">
-              <i className="fa-solid fa-spell-check"></i> English dictionary
-            </h6>
+        <div className="row justify-content-evenly gap-4">
+          <div className="col-5">
+            <img className="img-thumbnail" alt="Redux app screenshot" src={`res/images/${repo}.webp`} />
           </div>
-          <div className="row justify-content-evenly gap-4">
-            <div className="col-5">
-              <img className="img-thumbnail" alt="Dictionary app screenshot" src="res/images/dictionary.webp" />
-            </div>
-            <div className="col-5" style={{ display: "grid", placeItems: "center", alignContent: "center" }}>
-              <h6 className="mb-3">Dictionary based on a public API</h6>
-              <a href="dictionary" className="btn btn-sm" target="_blank" aria-label="Redux dashboard">
-                Try it
-              </a>
-            </div>
-            <div className="col mt-4 mb-1">
-              <a
-                className="btn"
-                style={{ fontSize: "0.8rem" }}
-                href="https://github.com/cornejobarraza/dictionary"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i className="fa-solid fa-code-branch" style={{ color: "var(--active)" }}></i> Repo
-              </a>
-            </div>
+          <div className="col-5" style={{ display: "grid", placeItems: "center", alignContent: "center" }}>
+            <h6 className="mb-3">{description}</h6>
+            <a href={repo} className="btn btn-sm" target="_blank" rel="noreferrer">
+              <i className="fa-solid fa-arrow-up-right-from-square" style={{ color: "var(--active)" }}></i>
+            </a>
           </div>
-        </div>
-      </div>
-      <div className="col-xl-3 col-lg-5">
-        <div className="row gap-5 justify-content-center mx-auto">
-          <div className="col-11 about">
-            <h6 className="m-0">
-              <i className="fa-solid fa-chart-simple"></i> Expenses chart
-            </h6>
-          </div>
-          <div className="row justify-content-evenly gap-4">
-            <div className="col-5">
-              <img className="img-thumbnail" alt="Expenses app screenshot" src="res/images/expenses.webp" />
-            </div>
-            <div className="col-5" style={{ display: "grid", placeItems: "center", alignContent: "center" }}>
-              <h6 className="mb-3">Dynamic weekly expenses chart</h6>
-              <a href="expenses" className="btn btn-sm" target="_blank" aria-label="Redux dashboard">
-                Check it
-              </a>
-            </div>
-            <div className="col mt-4 mb-1">
-              <a
-                className="btn"
-                style={{ fontSize: "0.8rem" }}
-                href="https://github.com/cornejobarraza/expenses"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i className="fa-solid fa-code-branch" style={{ color: "var(--active)" }}></i> Repo
-              </a>
-            </div>
+          <div className="col mt-4 mb-1">
+            <a
+              className="btn"
+              style={{ fontSize: "0.8rem" }}
+              href={`https://github.com/cornejobarraza/${repo}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-solid fa-code-branch" style={{ color: "var(--active)" }}></i>
+            </a>
           </div>
         </div>
       </div>
