5c5687ca1bd24553412ee89c4e1dbbdf9dc43467
David <davidcornejo1@live.com>
2022-12-17 22:28:44 -0600

    Updated project functional component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,20 +3,35 @@ export { Gallery };
 function Gallery() {
   return (
     <div className="row gap-5 justify-content-center text-center mx-auto">
-      <Project name="User dashboard" description="CRUD app using Redux Toolkit and Firebase" repo="redux" />
-      <Project name="English dictionary" description="Dictionary based on a public API" repo="dictionary" />
-      <Project name="Expenses chart" description="Weekly expenses chart with dynamic data" repo="expenses" />
+      <Project
+        icon="fa-address-card"
+        name="User dashboard"
+        description="CRUD app using Redux Toolkit and Firebase"
+        repo="redux"
+      />
+      <Project
+        icon="fa-spell-check"
+        name="English dictionary"
+        description="Dictionary based on a public API"
+        repo="dictionary"
+      />
+      <Project
+        icon="fa-chart-simple"
+        name="Expenses chart"
+        description="Weekly expenses chart with dynamic data"
+        repo="expenses"
+      />
     </div>
   );
 }
 
-function Project({ name, description, repo }) {
+function Project({ icon, name, description, repo }) {
   return (
     <div className="col-xl-3 col-lg-5">
       <div className="row gap-5 justify-content-evenly mx-auto">
         <div className="col-11 about">
           <h6 className="m-0">
-            <i className="fa-solid fa-id-card"></i> {name}
+            <i className={`fa-solid ${icon}`}></i> {name}
           </h6>
         </div>
         <div className="row justify-content-evenly gap-4">
